323ebb5c932e210540cd306731fe7e30a91bddad
Gokul nath <gokulnaths@applieddatafinance.com>
2021-07-03 11:50:12 +0530

    Chaged to textfield for content

--- daytoday/models.py
+++ daytoday/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Daily(models.Model):
     date = models.DateField(null=False)
-    content = models.CharField(max_length=30000, null=False)
+    content = models.TextField(max_length=30000, null=False)
     created_date = models.DateTimeField(timezone.now())
     updated_date = models.DateTimeField(auto_now=True)
 
